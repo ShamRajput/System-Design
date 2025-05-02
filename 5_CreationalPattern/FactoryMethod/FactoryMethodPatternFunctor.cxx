399210b51213d8864b6678a72f67527fd1df782f
Shamkumar Rajput <shamkumar.rajput@hotmail.com>
2025-05-02 13:16:30 +0000

    Behavioural Pattern and Creational patten class diagram added

---
+++ 5_CreationalPattern/FactoryMethod/FactoryMethodPatternFunctor.cxx
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <functional>
+#include <memory>
+#include <stdexcept>
+
+class IShape
+{
+public:
+    virtual void draw() = 0;
+    virtual ~IShape() = default;
+};
+
+class Circle : public IShape
+{
+public:
+    void draw() override {
+        std::cout << "Drawing Circle" << std::endl;
+    }
+};
+class Square : public IShape
+{
+public:
+    void draw() override {
+        std::cout << "Drawing Square" << std::endl;
+    }
+};
+
+
+class IShapeFactory final
+{
+   inline static std::function<std::unique_ptr<IShape>()> mCreateShape;
+public:
+    IShapeFactory(const IShapeFactory&) = delete;
+    IShapeFactory& operator=(const IShapeFactory&) = delete;
+    IShapeFactory(IShapeFactory&&) = delete;
+    IShapeFactory& operator=(IShapeFactory&&) = delete;
+    static void registerFactory(const std::function<std::unique_ptr<IShape>()>& factory) {
+        mCreateShape = factory;
+    }
+    static std::unique_ptr<IShape> getInstance() {
+        if (mCreateShape) {
+            return mCreateShape();
+        }
+        return nullptr;
+    }
+};
+
+int main() {
+    
+    auto c = []()-> std::unique_ptr<IShape> {
+        return std::make_unique<Circle>();
+    };
+
+    IShapeFactory::registerFactory(c);
+
+    std::unique_ptr<IShape> shape = IShapeFactory::getInstance();
+    shape->draw();
+    
+   
+    
+    return 0;
+}
